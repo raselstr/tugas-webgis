0ff3f7a58ca18429a636724e5dfdb596fcc3805c
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-08-05 11:00:51 +0700

    Pemetaan Coordinat

--- application/models/M_lokasi.php
+++ application/models/M_lokasi.php
@@ -3,9 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_lokasi extends CI_Model {
 
+	// Simpan data
 	public function input($data)
 	{
 		$this->db->insert('tbl_lokasi', $data);
         
+	}
+
+	// Ambil data dari database
+	public function allData()
+	{
+		db->
 	}
 }
