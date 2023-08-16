c4f14ca849fd1fecb32ee09f4680cdc3be0351d3
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-08-17 02:17:18 +0700

    Tambah Info di Home

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -1,1 +1,42 @@
-ini home
+<div class="row">
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <center><H1>BIODATA MAHASISWA</H1></center><br>
+                <table class="table table-hover">
+                    <tr>
+                        <th>NAMA</th>
+                        <td>:</td>
+                        <td>RAHMAD SALEH SITORUS</td>
+                    </tr>
+                    <tr>
+                        <th>NIM</th>
+                        <td>:</td>
+                        <td>222210612</td>
+                    </tr>
+                    <tr>
+                        <th>JURUSAN</th>
+                        <td>:</td>
+                        <td>Sistem Informasi</td>
+                    </tr>
+                    <tr>
+                        <th>KELAS</th>
+                        <td>:</td>
+                        <td>Transfer</td>
+                    </tr>
+                    <tr>
+                        <th>MATA KULIAH</th>
+                        <td>:</td>
+                        <td>Sistem Informasi Geografis</td>
+                    </tr>
+                    <tr>
+                        <th>DOSEN</th>
+                        <td>:</td>
+                        <td>Bpk. MARDALIUS, M.Kom</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
